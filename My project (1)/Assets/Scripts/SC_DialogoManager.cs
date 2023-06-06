b3d787c7dd7c78bf10cfbe108db1b377319eb061
Camelcompact99 <vazquez.tome.alejandro@gmail.com>
2023-06-06 17:30:19 +0200

    yeah

---
+++ My project (1)/Assets/Scripts/SC_DialogoManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogoManager : MonoBehaviour
+{
+    public TMP_Text textoDialogo;
+
+    private string[] dialogos;
+
+    private int indiceActual;
+
+    private void Start()
+    {
+        dialogos = new string[]
+        {
+            "Diálogo 1",
+            "Diálogo 2",
+            "Diálogo 3"
+        };
+
+        indiceActual = 0;
+
+        textoDialogo.text = dialogos[indiceActual];
+    }
+
+    public void MostrarSiguienteDialogo()
+    {
+        indiceActual++;
+
+        if (indiceActual < dialogos.Length)
+        {
+            textoDialogo.text = dialogos[indiceActual];
+        }
+        else
+        {
+            textoDialogo.text = "Fin del diálogo";
+        }
+    }
+}
+
